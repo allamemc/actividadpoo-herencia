1db01bcf6e7b164ea237f2afd8097dae97024652
allamemc <allamemc@gmail.com>
2022-11-29 13:51:41 +0100

    Initial commit

---
+++ practica.py
@@ -0,0 +1,13 @@
+class Producto():
+    def __init__(self,nombre,unidades,precio):
+        self.nombre=nombre
+        self.unidades=unidades
+        self.precio=precio
+    def total(self):
+        print(f'Total:{self.unidades*self.precio}€')
+
+producto1=Producto('camisa',15,9.95)
+producto2=Producto('chaquetas',20,19.95)
+
+producto1.total()
+producto2.total()
